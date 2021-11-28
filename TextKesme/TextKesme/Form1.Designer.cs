81edccdde4e156d7b31a9dc2c2bb06f9b2bd30a2
mehmet küçük <mehmetkucuk567@gmail.com>
2021-11-28 11:18:33 +0300

    C# ile text kesme
    Belirlenen özellikler ile text kesme

---
+++ TextKesme/TextKesme/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace TextKesme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.ekle_lis = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kesme";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(713, 265);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Dosya Açma";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 237);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(13, 256);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(694, 182);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(713, 333);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "ListBox a Aktar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ekle_lis
+            // 
+            this.ekle_lis.FormattingEnabled = true;
+            this.ekle_lis.Location = new System.Drawing.Point(13, 12);
+            this.ekle_lis.Name = "ekle_lis";
+            this.ekle_lis.Size = new System.Drawing.Size(775, 238);
+            this.ekle_lis.TabIndex = 8;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(775, 238);
+            this.listBox1.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.ekle_lis);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox ekle_lis;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TextKesme/TextKesme/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace TextKesme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string path;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog txtac = new OpenFileDialog();
+            txtac.Title = "Metin Belgesi Aç";
+            txtac.Filter = "Metin Dosyaları(*.txt) | *.txt";
+            if(txtac.ShowDialog()==DialogResult.OK)
+            {
+                path = txtac.FileName;
+                StreamReader oku = new StreamReader(txtac.FileName);
+                richTextBox1.Text = oku.ReadToEnd();
+                oku.Close();
+            }
+            else
+            {
+                return;
+            }
+        }
+        public string yazi;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            yazi = richTextBox1.Text;
+            yazi = yazi.Replace("&#231;", "ç").Replace("&#246;", "ö").Replace("&#220;", "Ü").Replace("&#214;", "Ö").Replace("&#199;", "Ç").Trim().Replace("&#252;", "ü").Replace('"', ' ').Trim();
+            yazi = yazi.Replace("try {", "").Replace("ol4Master.AddPoi(KoordinatDegistir(", "").Replace(", , &lt;b&gt;", "").Replace("-", "").Replace("}", "").Replace("catch (err) { ", "").Replace("console.log(", "").Replace(")", "").Replace("&lt;/br&gt;767,", "").Replace("/Content/img/icon-bus.png);", "").Replace("&lt;/b&gt;", "").Replace("Ge&#231;en Hatlar &lt;/br&gt;767, ", "").Replace("         ", "").Replace("    ", "").Replace("              ", "").Replace("                  ", "").Replace("   ", "").Replace(", , &lt;b&gt;", "").Replace("Hatlar ", "");
+            yazi = yazi.Replace("&lt;/br&gt;775, ", "").Replace("/Content/img/iconbus.png;", "").Replace("&lt;/br&gt;775,778, ", "").Replace("&lt;/br&gt;729,775, ", "").Replace("&lt;/br&gt;106,729,775,792, ", "");
+            yazi = yazi.Replace("&lt;/br&gt;729,775,776,789,792,804,808,814,829, ", "").Replace("&lt;/br&gt;106,729,775,776,792, ", "").Replace("&lt;/br&gt;106,708,729,775,776,792,808, ", "").Replace("&lt;/br&gt;707,729,775,776,789,792,804,808,829, ", "");
+            yazi = yazi.Replace("&lt;/br&gt;106,708,729,775,776,792,808,814, ", "").Replace("&lt;/br&gt;Geçen ", "").Replace("&lt;/br&gt;776,778,829, ", "").Replace("&lt;b&gt;", "").Trim();
+            yazi = yazi.Replace("\n\n", "");
+            if(yazi.Contains("&#")==true)
+            {
+                MessageBox.Show("Unicode bulundu kontrol ediniz. Dosya yolu:\n"+path,"HATA",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            richTextBox2.Text = yazi;
+            ekle_lis.BringToFront();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DosyaOkuma();
+        }
+        int sayac;
+        public string txt;
+        public void DosyaOkuma()
+        {
+            string[] Deger = Regex.Split(richTextBox2.Text, "\n");
+            foreach (string item in Deger)
+            {
+                ekle_lis.Items.Add(item);
+                sayac += 1;
+            }
+
+            string[] listBoxItem = new string[ekle_lis.Items.Count];
+
+            for (int i = 0; i < ekle_lis.Items.Count; i++)
+            {
+                listBoxItem[i] = ekle_lis.Items[i].ToString();
+
+                if (i % 3 == 0)
+                {
+                    listBoxItem[i] = ekle_lis.Items[i].ToString().Replace("&lt;/br&gt;","").Replace("1","").Replace("2", "").Replace("3", "").Replace("4", "").Replace("5", "").Replace("6", "").Replace("7", "").Replace("8", "").Replace("9", "").Replace("0", "").Replace(",", "").Replace("&lt;b&gt;", "").Replace(";","").Replace("/Content/img/iconbus.png ", "").Trim().Remove(0, 3).Trim();
+                    
+                }
+                listBox1.Items.Add(listBoxItem[i]);
+            }
+            listBox1.BringToFront();
+            File.WriteAllLines(path, listBox1.Items.Cast<string>().ToArray());
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval+=10;
+            if(timer1.Interval>=200)
+            {
+                Application.Restart();
+            }
+        }
+    }
+}
